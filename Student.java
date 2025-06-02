2acb49857cbc549c4adb728284eae59a227c10ef
saivenn <167971383+saivenn@users.noreply.github.com>
2025-06-02 14:55:44 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,41 @@
+public class Student {
+    private int id;
+    private String name;
+    private String grade;
+
+    public Student(int id, String name, String grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Grade: " + grade;
+    }
+}
